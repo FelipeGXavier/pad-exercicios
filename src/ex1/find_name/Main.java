7eeb44bae1fa7373cf050ae08453e29d6ca53cf0
FelipeGXavier <felipexavier20015@gmail.com>
2022-08-22 18:33:25 -0300

    feat: finish exercices and add pdf report

--- src/ex1/find_name/Main.java
+++ src/ex1/find_name/Main.java
@@ -1,7 +1,5 @@
 package ex1.find_name;
 
-import util.UtilReader;
-
 import java.util.List;
 
 public class Main {
@@ -10,18 +8,12 @@ public class Main {
 
         // Sequential finder
         Finder sequentialFinder = new SequentialFinder();
-        long startSequential = System.currentTimeMillis();
         List<FindResult> result = sequentialFinder.find("Rodrigo");
-        long endSequential = System.currentTimeMillis();
-        //sequentialFinder.print(result);
-        System.out.println("Sequential - Elapsed Time in milli seconds: "+ (endSequential-startSequential));
+        sequentialFinder.print(result);
 
         // ConcurrentFinder
         Finder concurrentFinder = new ConcurrentFinder();
-        long startConcurrent = System.currentTimeMillis();
         List<FindResult> concurrentResult = concurrentFinder.find("Rodrigo");
-        long endConcurrent = System.currentTimeMillis();
-        //concurrentFinder.print(concurrentResult);
-        System.out.println("Concurrent - Elapsed Time in milli seconds: "+ (endConcurrent-startConcurrent));
+        concurrentFinder.print(concurrentResult);
     }
 }
